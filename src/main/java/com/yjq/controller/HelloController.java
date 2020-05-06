bbac8896de807a06203ddbad706bd29edfe11260
yaojiuqi <393072365@qq.com>
2020-05-06 21:22:31 +0800

    ssmxml-template

---
+++ src/main/java/com/yjq/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.yjq.controller;
+
+import com.yjq.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+    @GetMapping(value = "/hello",produces = "text/html;charset=utf-8")
+    public String hello(){
+        return helloService.sayHello();
+    }
+}
+
---
+++ src/main/java/com/yjq/service/HelloService.java
@@ -0,0 +1,11 @@
+package com.yjq.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String sayHello() {
+        return "hello Java 极客技术";
+    }
+}
